65d2c60b1476df1366b189a81d4c9e7af54144bd
S <s>
2015-05-05 15:21:30 +0800

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -72,7 +72,7 @@ public class Breakout extends GraphicsProgram{
 		
 		
 		
-		for (int i = 2; i != 0 ; i--){
+		for (int i = 2; i >= 0 ; i--){
 			for (int j = 0; j <= NBRICKS_PER_ROW ; j++){
 				double x = j* BRICK_WIDTH + j*BRICK_SEP;  
 				int y = BRICK_Y_OFFSET;
