4bfa77066969bde5bd51b8966ef1ca8922b20d4c
S <s>
2015-05-05 10:56:06 +0800

    {"type":"TI", "runs":"2"}

--- Breakout.java
+++ Breakout.java
@@ -115,7 +115,7 @@ public class Breakout extends GraphicsProgram{
 			
 			
 			
-			if (ball.getY() + ball.getHeight() > getHeight()){
+			if (ball.getY() + ball.getHeight() >= getHeight()){
 				vy = -vy;
 			}
 			
