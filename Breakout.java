ead56e682703f43cfb856392809ebd8132ef4ae0
S <s>
2015-05-05 11:08:28 +0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -67,7 +67,7 @@ public class Breakout extends GraphicsProgram{
 		add(paddle);
 		//add(bricks);
 		add(ball);
-		//moveBall();
+		moveBall();
 		addMouseListeners();
 	}		
 	
@@ -97,7 +97,55 @@ public class Breakout extends GraphicsProgram{
 	}
 	
 	/*set vx,vy to make ball move*/
+	private void moveBall(){
+		vy = 3.0;
+		RandomGenerator rgen = RandomGenerator.getInstance();
+		vx = rgen.nextDouble(1.0, 3.0);
+		if (rgen.nextBoolean(0.5)){
+			vx = -vx;
+		}	
+		while(true){
+			ball.move(vx, vy);
+			pause(PAUSE_TIME);
+			if (ball.getY() + ball.getHeight() >= getHeight()){
+				vy = -vy;
+			}
+			if (ball.getY() <= 0){
+				vy = -vy;
+			}
+			if (ball.getX() + ball.getWidth() >= getWidth()){
+				vx = -vx;
+			}
+			if (ball.getX() <= 0){
+				vx = -vx;
+			}
+		}
+	}
 	
+	
+	
+	private GObject getCollidingObject(){
+		if (getElementAt(ball.getX(),ball.getY())!= null){
+			/**strange*/
+			GObject collider = getElementAt(ball.getX(),ball.getY());
+			return collider;
+		}
+		else if (getElementAt(ball.getX()+ 2*BALL_RADIUS,ball.getY())!= null){
+			GObject collider = getElementAt(ball.getX()+ 2*BALL_RADIUS,ball.getY());	
+			return collider;
+		}
+		else if (getElementAt(ball.getX(),ball.getY()+ 2*BALL_RADIUS)!= null){
+			GObject collider = getElementAt(ball.getX(),ball.getY()+ 2*BALL_RADIUS);
+			return collider;
+		}
+		else if (getElementAt(ball.getX()+ 2*BALL_RADIUS,ball.getY()+ 2*BALL_RADIUS)!= null){
+			GObject collider = getElementAt(ball.getX()+ 2*BALL_RADIUS,ball.getY()+ 2*BALL_RADIUS);	
+			return collider;
+		}
+		else{
+			return null;
+		}
+	}
 }
 		
 
